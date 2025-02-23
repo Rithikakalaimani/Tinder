3a1c67602dbcc205708eda3550cb6cacd51c05d8
Rithikakalaimani <123987662+Rithikakalaimani@users.noreply.github.com>
2025-02-24 01:01:42 +0530

    fetched and added user

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,8 +1,28 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
+import axios from "axios"
+import {useDispatch} from "react-redux"
+import {addUser} from "../Utils/userSlice"
+import { BASE_URL } from "../Utils/constant"
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const fetchUser = async ()=>{
+    try{ 
+        const response = await axios.get(BASE_URL+"/profile/view",{
+        withCredentials:true
+      });
+      dispatch(addUser(response.data));
+    }
+    catch(err)
+    {
+      console.error(err);
+    }  
+  }
+  // useEffect(()=>{
+  //   fetchUser();
+  // },[]);
   return (
     <div>
       <Navbar/>
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import { useDispatch } from 'react-redux'
+import { addUser } from '../Utils/userSlice'
 import { useNavigate } from 'react-router-dom'
 import { BASE_URL } from '../Utils/constant'
 
